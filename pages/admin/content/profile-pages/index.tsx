c17a14ea9a4cd4986c1fbb9d89d1cc2f79b0f0fa
boonyanone <boonyanone@gmail.com>
2026-02-08 09:12:22 +0700

    refactor: simplify admin pages to use client-side rendering for improved performance

--- pages/admin/content/profile-pages/index.tsx
+++ pages/admin/content/profile-pages/index.tsx
@@ -1,34 +1,5 @@
 import ProfileContentIndexPage from '../../../../src/features/admin/pages/content/profile-pages';
-import { GetServerSideProps } from 'next';
-import nookies from 'nookies';
-import { adminAuth, adminFirestore } from '../../../../firebase-admin';
 
 export default function AdminProfileContentIndexRoute(props: any) {
     return <ProfileContentIndexPage />;
 }
-
-export const getServerSideProps: GetServerSideProps = async (context) => {
-    try {
-        const cookies = nookies.get(context);
-        const token = cookies.token;
-
-        if (!token) {
-            return { redirect: { destination: "/login", permanent: false } };
-        }
-
-        const decodedToken = await adminAuth.verifyIdToken(token);
-        const uid = decodedToken.uid;
-
-        // Check Firestore (Hybrid Mode Support)
-        const userDoc = await adminFirestore.collection('users').doc(uid).get();
-        const userData = userDoc.data();
-
-        if (!userDoc.exists || userData?.role !== 'admin') {
-            return { redirect: { destination: "/", permanent: false } };
-        }
-
-        return { props: {} };
-    } catch (error) {
-        return { redirect: { destination: "/login", permanent: false } };
-    }
-};
--- pages/admin/plans.tsx
+++ pages/admin/plans.tsx
@@ -1,34 +1,5 @@
 import ModulesPage from '../../src/features/admin/pages/ModulesPage';
-import { GetServerSideProps } from 'next';
-import nookies from 'nookies';
-import { adminAuth, adminFirestore } from '../../firebase-admin';
 
 export default function AdminPlansRoute(props: any) {
     return <ModulesPage />;
 }
-
-export const getServerSideProps: GetServerSideProps = async (context) => {
-    try {
-        const cookies = nookies.get(context);
-        const token = cookies.token;
-
-        if (!token) {
-            return { redirect: { destination: "/login", permanent: false } };
-        }
-
-        const decodedToken = await adminAuth.verifyIdToken(token);
-        const uid = decodedToken.uid;
-
-        // Check Firestore (Hybrid Mode Support)
-        const userDoc = await adminFirestore.collection('users').doc(uid).get();
-        const userData = userDoc.data();
-
-        if (!userDoc.exists || userData?.role !== 'admin') {
-            return { redirect: { destination: "/", permanent: false } };
-        }
-
-        return { props: {} };
-    } catch (error) {
-        return { redirect: { destination: "/login", permanent: false } };
-    }
-};
